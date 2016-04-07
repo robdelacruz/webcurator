0e82154c07cf8b1e0de009edcafdce0ad361a4b0
robdelacruz <robdelacruz@gmail.com>
2016-04-08 07:54:34 +0800

    Use article instead of post. Differentiate between page and individual article template.

--- webc.pl
+++ webc.pl
@@ -8,6 +8,9 @@ use DateTime;
 use DateTime::Format::ISO8601;
 use File::Path;
 
+###
+### Helper functions
+###
 sub trim {
 	my $s = shift;
 	$s =~ s/^\s+|\s+$//g;
@@ -30,48 +33,56 @@ sub datetime_from_str {
 
 sub read_template_file {
 	my $template_filename = shift;
-	open my $hposttemplate, '<', $template_filename
+	open my $htemplate, '<', $template_filename
 		or die "Can't open '$template_filename': $!\n";
 
 	local $/;
-	my $template_str = <$hposttemplate>;
-	close $hposttemplate;
+	my $template_str = <$htemplate>;
+	close $htemplate;
 	return $template_str;
 }
 
-my $post_template = &read_template_file('post_template.html');
-my $archives_template = &read_template_file('archive_template.html');
-my %posts;
-my @sorted_posts_keys;
+###
+### Templates
+###
+my $page_article_template = &read_template_file('page_article_template.html');
+my $article_template = &read_template_file('article_template.html');
+my $page_archives_template = &read_template_file('page_archives_template.html');
+
+###
+### Global structures
+###
+my %articles;
+my @sorted_articles_keys;
 
 &main();
 
 ### Start here ###
 sub main {
-	&process_post_files(@ARGV);
+	&process_article_files(@ARGV);
 
 	my $output_dir = 'site';
 
 	&clear_outputdir($output_dir);
 
-	print "Writing posts html to $output_dir...\n";
-	&write_post_html_files($output_dir);
+	print "Writing articles html to $output_dir...\n";
+	&write_article_html_files($output_dir);
 
-	print "Writing archive html page to $output_dir...\n";
-	&write_archive_html_file($output_dir);
+	print "Writing archives html page to $output_dir...\n";
+	&write_archives_html_file($output_dir);
 }
 
 # Return whether filename has WEBC signature
 sub is_webc_file {
-	my ($post_filename) = @_;
+	my ($article_filename) = @_;
 
-	my $hpostfile;
-	if (!open $hpostfile, '<', $post_filename) {
-		warn "Error opening '$post_filename': $!\n";
+	my $harticlefile;
+	if (!open $harticlefile, '<', $article_filename) {
+		warn "Error opening '$article_filename': $!\n";
 		return 0;
 	}
 
-	my $first_line = <$hpostfile>;
+	my $first_line = <$harticlefile>;
 	return 0 if !$first_line;
 
 	chomp($first_line);
@@ -80,30 +91,32 @@ sub is_webc_file {
 		$is_webc = 1;
 	}
 
-	close $hpostfile;
+	close $harticlefile;
 	return $is_webc;
 }
 
-# Return ($datetime, $title_str, $title_filename) from %posts hash key
-sub parse_postkey {
-	my $postkey = shift;
-	my ($dt_str, $title) = split(/_/, $postkey);
+# Return ($datetime, $title_str, $title_filename) from %articles hash key
+sub parse_article_key {
+	my $article_key = shift;
+
+	my $sep = '___';
+	my ($author, $dt_str, $title) = split(/$sep/, $article_key);
 
 	my $dt = datetime_from_str($dt_str);
 	my $title_filename = "$title.html";
 	$title =~ s/\+/ /g;
 
-	return ($dt, $title, $title_filename);
+	return ($author, $dt, $title, $title_filename);
 }
 
-# Parse list of post text files into hash structure
-sub process_post_files {
+# Parse list of article text files into hash structure
+sub process_article_files {
 	my @input_files = @_;
 
-	foreach my $post_filename (@input_files) {
-		print "Processing file: $post_filename... ";
+	foreach my $article_filename (@input_files) {
+		print "Processing file: $article_filename... ";
 
-		if (! -e $post_filename) {
+		if (! -e $article_filename) {
 			print "File not found.\n";
 			next;
 		} elsif (! -r _) {
@@ -112,43 +125,45 @@ sub process_post_files {
 		} elsif (! -T _) {
 			print "Invalid file.\n";
 			next;
-		} elsif (!&is_webc_file($post_filename)) {
+		} elsif (!&is_webc_file($article_filename)) {
 			print "Missing WEBC n.n header line.\n";
 			next;
 		}
 
 		print "\n";
 
-		&process_post_file($post_filename);
+		&process_article_file($article_filename);
 	}
 
-	@sorted_posts_keys = sort {
-		my ($dt_a) = &parse_postkey($a);
-		my ($dt_b) = &parse_postkey($b);
+	@sorted_articles_keys = sort {
+		my ($author_a, $dt_a) = &parse_article_key($a);
+		my ($author_b, $dt_b) = &parse_article_key($b);
 
-		if (!$dt_a || !$dt_b) {
-			return 0;
+		my $result_dt = $dt_a <=> $dt_b;
+		if ($result_dt == 0) {
+			return $author_a <=> $author_b;
+		} else {
+			return $result_dt;
 		}
-		return $dt_a <=> $dt_b;
-	} keys %posts;
+	} keys %articles;
 }
 
-# Parse one post text file and add entry to hash structure
-sub process_post_file {
-	my ($post_filename) = @_;
+# Parse one article text file and add entry to hash structure
+sub process_article_file {
+	my ($article_filename) = @_;
 
-	my $hpostfile;
-	if (!open $hpostfile, '<', $post_filename) {
-		warn "Error opening '$post_filename': $!\n";
+	my $harticlefile;
+	if (!open $harticlefile, '<', $article_filename) {
+		warn "Error opening '$article_filename': $!\n";
 		return;
 	}
 
 	# Skip WEBC n.n line
-	my $first_line = <$hpostfile>;
+	my $first_line = <$harticlefile>;
 
 	# Add each 'HeaderKey: HeaderVal' line to %headers
 	my %headers;
-	while (my $line = <$hpostfile>) {
+	while (my $line = <$harticlefile>) {
 		chomp($line);
 
 		last if length($line) == 0;
@@ -161,31 +176,35 @@ sub process_post_file {
 		$headers{$k} = $v;
 	}
 
-	# Add each post body to %posts under key '<datetime>_<title>'
-	my $post_date = $headers{'Date'};
-	my $post_title = $headers{'Title'};
+	# Add each article content to %articles under key '<datetime>_<title>'
+	my $article_date = $headers{'Date'};
+	my $article_title = $headers{'Title'};
+	my $article_author = $headers{'Author'};
 
-	if (defined $post_date && defined $post_title) {
-		$post_title =~ s/\s/+/g;
+	if (defined $article_date && defined $article_title && defined $article_author) {
+		$article_title =~ s/\s/+/g;
 
-		my $post_dt = datetime_from_str($post_date);
-		if ($post_dt) {
-			# Add to posts hash.
-			my $post_key = $post_dt->datetime() . "_" . $post_title;
-			my $post_body;
+		my $article_dt = datetime_from_str($article_date);
+		if ($article_dt) {
+			# Add to articles hash.
+			my $sep = '___';
+			my $article_key =
+				$article_author . $sep . $article_dt->datetime() . $sep . $article_title;
+
+			my $article_content;
 			{
 				local $/;
-				$post_body = <$hpostfile>;
+				$article_content = <$harticlefile>;
 			}
-			$posts{$post_key} = $post_body;
+			$articles{$article_key} = $article_content;
 		} else {
-			print "Skipping $post_filename. Invalid header date: '$post_date'\n";
+			print "Skipping $article_filename. Invalid header date: '$article_date'\n";
 		}
 	} else {
-		print "Skipping $post_filename. Date or Title not defined in header.\n";
+		print "Skipping $article_filename. Date, Author, or Title missing in Header.\n";
 	}
 
-	close $hpostfile;
+	close $harticlefile;
 }
 
 sub clear_outputdir {
@@ -204,65 +223,81 @@ sub write_to_file {
 	close $houtfile;
 }
 
-# Given posts hash structure, output the html file list
-sub write_post_html_files {
+sub construct_article_html {
+	my ($article_author, $article_dt, $article_title, $article_content) = @_;
+
+	my $article_html = $article_template;
+
+	$article_html =~ s/{title}/$article_title/g;
+	$article_html =~ s/{author}/$article_author/g;
+
+	my $dt_formattedstr = $article_dt->year . "/" .
+						  $article_dt->month . "/" .
+						  $article_dt->day;
+	$article_html =~ s/{date}/$dt_formattedstr/g;
+
+	$article_html =~ s/{article_content}/$article_content/g;
+
+	return $article_html;
+}
+
+# Given articles hash structure, output the html file list
+sub write_article_html_files {
 	my $outdir = shift;
 
 	my $prev_k;
 	my $next_k;
 
-	for my $i (0..$#sorted_posts_keys) {
-		my $k = $sorted_posts_keys[$i];
-		$next_k = $sorted_posts_keys[$i+1];
+	for my $i (0..$#sorted_articles_keys) {
+		my $k = $sorted_articles_keys[$i];
+		$next_k = $sorted_articles_keys[$i+1];
 
-		my ($post_dt, $post_title, $post_title_filename) = parse_postkey($k);
+		my ($article_author, $article_dt, $article_title, $article_title_filename)
+			= parse_article_key($k);
 
-		if (!$post_dt) {
-			print "Can't parse datetime in '$post_title'. Skipping.\n";
+		if (!$article_dt) {
+			print "Can't parse datetime in '$article_title'. Skipping.\n";
 			next;
 		}
 
-		my $post_html = $post_template;
-		$post_html =~ s/{title}/$post_title/g;
+		my $article_html =
+			&construct_article_html($article_author, $article_dt, $article_title, $articles{$k});
 
-		my $dt_formattedstr = $post_dt->year . "/" . $post_dt->month . "/" . $post_dt->day;
-		$post_html =~ s/{date}/$dt_formattedstr/g;
-
-		my $post_body = $posts{$k};
-		$post_html =~ s/{post}/$post_body/g;
-
-		my $prev_post_href = '';
+		my $prev_article_href = '';
 		if ($prev_k) {
-			my ($prev_dt, $prev_title, $prev_title_filename) = parse_postkey($prev_k);
-			$prev_post_href = "Previous: <a href='$prev_title_filename'>$prev_title</a>";
+			my ($author, $prev_dt, $prev_title, $prev_title_filename) = parse_article_key($prev_k);
+			$prev_article_href = "Previous: <a href='$prev_title_filename'>$prev_title</a>";
 		}
-		my $next_post_href = '';
+		my $next_article_href = '';
 		if ($next_k) {
-			my ($next_dt, $next_title, $next_title_filename) = parse_postkey($next_k);
-			$next_post_href = "Next: <a href='$next_title_filename'>$next_title</a>";
+			my ($author, $next_dt, $next_title, $next_title_filename) = parse_article_key($next_k);
+			$next_article_href = "Next: <a href='$next_title_filename'>$next_title</a>";
 		}
 
-		$post_html =~ s/{prev_post_href}/$prev_post_href/g;
-		$post_html =~ s/{next_post_href}/$next_post_href/g;
+		my $page_article_html = $page_article_template;
+		$page_article_html =~ s/{title}/$article_title/g;
+		$page_article_html =~ s/{prev_article_href}/$prev_article_href/g;
+		$page_article_html =~ s/{next_article_href}/$next_article_href/g;
+		$page_article_html =~ s/{article}/$article_html/g;
 
-		my $outfilename = "$outdir/$post_title_filename";
+		my $outfilename = "$outdir/$article_title_filename";
 		print "Writing to file '$outfilename'...\n";
-		&write_to_file($outfilename, $post_html);
+		&write_to_file($outfilename, $page_article_html);
 
 		$prev_k = $k;
 	}
 }
 
-# Given posts hash structure, output the html file list
-sub write_archive_html_file {
+# Given articles hash structure, output the html file list
+sub write_archives_html_file {
 	my $outdir = shift;
 
-	my $archives_html = $archives_template;
+	my $page_archives_html = $page_archives_template;
 
 	my $archive_content;
 	my $year = -1;
-	foreach my $k (@sorted_posts_keys) {
-		my ($dt, $title, $title_filename) = parse_postkey($k);
+	foreach my $k (@sorted_articles_keys) {
+		my ($author, $dt, $title, $title_filename) = parse_article_key($k);
 
 		my $yearMarkup;
 		if ($year != $dt->year) {
@@ -272,15 +307,15 @@ sub write_archive_html_file {
 			$yearMarkup = "";
 		}
 
-		my $post_href = "<a href='$title_filename'>$title</a>\n";
+		my $article_href = "<a href='$title_filename'>$title</a>\n";
 
-		$archive_content .= $yearMarkup . $post_href;
+		$archive_content .= $yearMarkup . $article_href;
 	}
 
-	$archives_html =~ s/{archive_content}/$archive_content/g;
+	$page_archives_html =~ s/{archive_content}/$archive_content/g;
 
 	my $archives_filename = 'archives.html';
 	my $outfilename = "$outdir/$archives_filename";
 	print "Writing to file '$outfilename'...\n";
-	&write_to_file($outfilename, $archives_html);
+	&write_to_file($outfilename, $page_archives_html);
 }
